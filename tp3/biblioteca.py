1effd35af73cb93cc649374a75f50414b92c7f91
matias-07 <mhuenul@fi.uba.ar>
2018-12-03 11:50:52 -0300

    Agrego vacaciones y exportar_kml

--- tp3/biblioteca.py
+++ tp3/biblioteca.py
@@ -5,61 +5,115 @@ from cola import Cola
 INFINITO = float("inf")
 
 def reconstruir_camino(destino, padres):
-	"""Devuelve una lista ordenada con el camino desde origen
-	hasta destino."""
-	camino = [destino]
-	v = destino
-	while padres[v] != None:
-		camino.append(padres[v])
-		v = padres[v]
-	camino.reverse()
-	return camino
+    """Devuelve una lista ordenada con el camino desde origen
+    hasta destino."""
+    camino = [destino]
+    v = destino
+    while padres[v] != None:
+        camino.append(padres[v])
+        v = padres[v]
+    camino.reverse()
+    return camino
 
 def obtener_camino_minimo(grafo, peso, origen, destino=None):
-	"""Recibe un grafo, la clave del vértice origen y la del destino,
-	el tipo de peso a tener en cuenta, y devuelve un camino mínimo
-	aplicando el algoritmo de Dijkstra. Si no se recibe un destino,
-	devuelve los caminos mínimos desde origen hasta todos los demás
-	vértices del grafo."""
-	distancias, padres = {}, {}
-	for v in grafo:
-		distancias[v] = INFINITO
-	distancias[origen] = 0
-	padres[origen] = None
-	q = Heap()
-	q.encolar(origen, distancias[origen])
-	while not q.esta_vacio():
-		v = q.desencolar()
-		if v == destino:
-			return (reconstruir_camino(destino, padres), distancias[v])
-		for w in grafo.obtener_adyacentes(v):
-			if distancias[v] + grafo.obtener_peso_union(v, w)[peso] < distancias[w]:
-				distancias[w] = distancias[v] + grafo.obtener_peso_union(v, w)[peso]
-				padres[w] = v
-				q.encolar(w, distancias[w])
-	return distancias, padres
+    """Recibe un grafo, la clave del vértice origen y la del destino,
+    el tipo de peso a tener en cuenta, y devuelve un camino mínimo
+    aplicando el algoritmo de Dijkstra. Si no se recibe un destino,
+    devuelve los caminos mínimos desde origen hasta todos los demás
+    vértices del grafo."""
+    distancias, padres = {}, {}
+    for v in grafo:
+        distancias[v] = INFINITO
+    distancias[origen] = 0
+    padres[origen] = None
+    q = Heap()
+    q.encolar(origen, distancias[origen])
+    while not q.esta_vacio():
+        v = q.desencolar()
+        if v == destino:
+            return (reconstruir_camino(destino, padres), distancias[v])
+        for w in grafo.obtener_adyacentes(v):
+            if distancias[v] + grafo.obtener_peso_union(v, w)[peso] < distancias[w]:
+                distancias[w] = distancias[v] + grafo.obtener_peso_union(v, w)[peso]
+                padres[w] = v
+                q.encolar(w, distancias[w])
+    return distancias, padres
 
 def escalas_minimas_bfs(grafo, origen, destino=None):
-	"""Recibe un grafo, la clave del vertice origen y la del destino,
-	y devuelve un camino con la minima cantidad de escalas aplicando
-	el algoritmo bfs. Si no se recibe un destino, devuelve los caminos
-	con menor cantidad de escalas desde origen hasta todos los demás
-	vértices del grafo."""
-	visitados = []
-	padres, orden = {}, {}
-	q = Cola()
-	visitados.append(origen)
-	padres[origen] = None
-	orden[origen] = 0
-	q.encolar(origen)
-	while not q.esta_vacia():
-		v = q.desencolar()
-		for w in grafo.obtener_adyacentes(v):
-			if w not in visitados:
-				visitados.append(w)
-				padres[w] = v
-				orden[w] = orden[v] + 1
-				q.encolar(w)
-			if w == destino:
-				return reconstruir_camino(destino, padres), orden[w]
-	return padres, orden
+    """Recibe un grafo, la clave del vertice origen y la del destino,
+    y devuelve un camino con la minima cantidad de escalas aplicando
+    el algoritmo bfs. Si no se recibe un destino, devuelve los caminos
+    con menor cantidad de escalas desde origen hasta todos los demás
+    vértices del grafo."""
+    visitados = []
+    padres, orden = {}, {}
+    q = Cola()
+    visitados.append(origen)
+    padres[origen] = None
+    orden[origen] = 0
+    q.encolar(origen)
+    while not q.esta_vacia():
+        v = q.desencolar()
+        for w in grafo.obtener_adyacentes(v):
+            if w not in visitados:
+                visitados.append(w)
+                padres[w] = v
+                orden[w] = orden[v] + 1
+                q.encolar(w)
+            if w == destino:
+                return reconstruir_camino(destino, padres), orden[w]
+    return padres, orden
+
+def obtener_viaje(grafo, origen, n):
+    """Recibe un grafo, un origen y un número entero n.
+    Devuelve un viaje de n lugares que comienza y finaliza
+    en origen."""
+    recorrido = [origen]
+    visitados = set()
+    if not _obtener_viaje(grafo, origen, n, recorrido, visitados):
+        return None
+    return recorrido
+
+def _obtener_viaje(grafo, origen, n, recorrido, visitados):
+    """Función auxiliar para obtener un viaje de n lugares."""
+    ultimo = recorrido[-1]
+    if len(recorrido) == n:
+        if origen not in grafo.obtener_adyacentes(ultimo):
+            return False
+        recorrido.append(origen)
+        return True
+    for v in grafo.obtener_adyacentes(ultimo):
+        if v in visitados:
+            continue
+        recorrido.append(v)
+        visitados.add(v)
+        if _obtener_viaje(grafo, origen, n, recorrido, visitados):
+            return True
+        recorrido.pop()
+        visitados.remove(v)
+    return False
+
+def exportar_archivo_kml(grafo, recorrido, archivo):
+    """Recibe una lista con un recorrido y un archivo, y exporta
+    a dicho archivo el recorrido en formato kml."""
+    archivo.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+    archivo.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
+    archivo.write('    <Document>\n')
+    for i in range(len(recorrido)):
+        coordenadas = grafo.obtener_dato(recorrido[i])
+        archivo.write('        <Placemark>\n')
+        archivo.write('            <name>{}</name>\n'.format(recorrido[i]))
+        archivo.write('            <Point>\n')
+        archivo.write('                <coordinates>{}</coordinates>\n'.format(coordenadas))
+        archivo.write('            </Point>\n')
+        archivo.write('        </Placemark>\n')
+    for i in range(len(recorrido) - 1):
+        origen = grafo.obtener_dato(recorrido[i])
+        destino = grafo.obtener_dato(recorrido[i+1])
+        archivo.write('        <Placemark>\n')
+        archivo.write('            <LineString>\n')
+        archivo.write('                <coordinates>{} {}</coordinates>\n'.format(origen, destino))
+        archivo.write('            </LineString>\n')
+        archivo.write('        </Placemark>\n')
+    archivo.write('    </Document>\n')
+    archivo.write('</kml>\n')
--- tp3/flycombi.py
+++ tp3/flycombi.py
@@ -9,109 +9,140 @@ PRECIO = 1
 VUELOS = 2
 
 def listar_operaciones():
-	"""Muestra por pantalla todas las operaciones disponibles"""
-	operaciones = ["camino_mas", "camino_escalas", "nueva_aerolinea", "vacaciones"]
-	for op in operaciones:
-		print(op)
+    """Muestra por pantalla todas las operaciones disponibles"""
+    operaciones = ["camino_mas", "camino_escalas", "nueva_aerolinea", "vacaciones"]
+    for op in operaciones:
+        print(op)
 
 def obtener_aeropuertos(grafo, ciudades, ruta_aeropuertos):
-	"""Recibe un grafo, un diccionario de ciudades, una ruta de archivo
-	csv de aeropuertos, y guarda los datos en el grafo y en el diccionario.
-	PRE: El archivo debe tener el formato ciudad,codigo,latitud,longitud."""
-	with open(ruta_aeropuertos, "r") as archivo:
-		reader = csv.reader(archivo)
-		for ciudad, codigo, latitud, longitud in reader:
-			ubicacion = (latitud, longitud)
-			grafo.agregar_vertice(codigo, (ciudad, ubicacion))
-			if ciudad not in ciudades:
-				ciudades[ciudad] = []
-			ciudades[ciudad].append(codigo)
+    """Recibe un grafo, un diccionario de ciudades, una ruta de archivo
+    csv de aeropuertos, y guarda los datos en el grafo y en el diccionario.
+    PRE: El archivo debe tener el formato ciudad,codigo,latitud,longitud."""
+    with open(ruta_aeropuertos, "r") as archivo:
+        reader = csv.reader(archivo)
+        for ciudad, codigo, latitud, longitud in reader:
+            ubicacion = ",".join([longitud, latitud])
+            grafo.agregar_vertice(codigo, ubicacion)
+            if ciudad not in ciudades:
+                ciudades[ciudad] = []
+            ciudades[ciudad].append(codigo)
 
 def obtener_vuelos(grafo, ruta_vuelos):
-	"""Recibe un grafo y una ruta a un archivo csv de vuelos,
-	y guarda los datos en el grafo.
-	PRE: El archivo debe tener el formato
-	aeropuerto_i,aeropuerto_j,tiempo,precio,vuelos."""
-	with open(ruta_vuelos, "r") as archivo:
-		reader = csv.reader(archivo)
-		for aeropuerto_i, aeropuerto_j, tiempo, precio, vuelos in reader:
-			pesos = (int(tiempo), int(precio), int(vuelos))
-			grafo.agregar_arista(aeropuerto_i, aeropuerto_j, pesos)
+    """Recibe un grafo y una ruta a un archivo csv de vuelos,
+    y guarda los datos en el grafo.
+    PRE: El archivo debe tener el formato
+    aeropuerto_i,aeropuerto_j,tiempo,precio,vuelos."""
+    with open(ruta_vuelos, "r") as archivo:
+        reader = csv.reader(archivo)
+        for aeropuerto_i, aeropuerto_j, tiempo, precio, vuelos in reader:
+            pesos = (int(tiempo), int(precio), int(vuelos))
+            grafo.agregar_arista(aeropuerto_i, aeropuerto_j, pesos)
 
 def get_camino_minimo(grafo, origen, destino, peso=None):
-	"""Llama a la funcion para obtener el camino minimo correspondiente segun
-	si se le pasa un peso o no"""
-	if peso == None: return b.escalas_minimas_bfs(grafo, origen, destino)
-	return b.obtener_camino_minimo(grafo, peso, origen, destino)
+    """Llama a la funcion para obtener el camino minimo correspondiente segun
+    si se le pasa un peso o no"""
+    if peso == None: return b.escalas_minimas_bfs(grafo, origen, destino)
+    return b.obtener_camino_minimo(grafo, peso, origen, destino)
 
 def camino_minimo(grafo, ciudades, parametros):
-	"""Recibe un grafo, un diccionario ciudad: lista_aeropuertos, y una lista
-	de parámetros. Imprime un camino mínimo desde el origen hasta el destino.
-	Devuelve un booleano indicando si el comando se ejecutó correctamente."""
-	if len(parametros) == 3:
-		peso, ciudad_origen, ciudad_destino = parametros
-		if peso != "barato" and peso != "rapido":
-			return False
-		if peso == "rapido": peso = TIEMPO
-		if peso == "barato": peso = PRECIO
-	elif len(parametros) == 2:
-		ciudad_origen, ciudad_destino = parametros
-		peso = None
-	else:
-		return False
+    """Recibe un grafo, un diccionario ciudad: lista_aeropuertos, y una lista
+    de parámetros. Imprime un camino mínimo desde el origen hasta el destino.
+    Devuelve un booleano indicando si el comando se ejecutó correctamente."""
+    if len(parametros) == 3:
+        peso, ciudad_origen, ciudad_destino = parametros
+        if peso != "barato" and peso != "rapido":
+            return False
+        if peso == "rapido": peso = TIEMPO
+        if peso == "barato": peso = PRECIO
+    elif len(parametros) == 2:
+        ciudad_origen, ciudad_destino = parametros
+        peso = None
+    else:
+        return False
 
-	if ciudad_origen not in ciudades or ciudad_destino not in ciudades:
-		return False
-	camino, distancia = None, None
-	for origen in ciudades[ciudad_origen]:
-		for destino in ciudades[ciudad_destino]:
-			c, d = get_camino_minimo(grafo, origen, destino, peso)
-			if not distancia or d < distancia:
-				camino, distancia = c, d
-	resultado = ""
-	for aeropuerto in camino:
-		resultado += "{} -> ".format(aeropuerto)
-	print(resultado.rstrip(" -> "))
-	return True
+    if ciudad_origen not in ciudades or ciudad_destino not in ciudades:
+        return False
+    camino, distancia = None, None
+    for origen in ciudades[ciudad_origen]:
+        for destino in ciudades[ciudad_destino]:
+            c, d = get_camino_minimo(grafo, origen, destino, peso)
+            if not distancia or d < distancia:
+                camino, distancia = c, d
+    print(" -> ".join(camino))
+    return camino
 
 def nueva_aerolinea(grafo, parametros):
-	pass
+    pass
 
-def vacaciones(grafo, parametros):
-	pass
+def vacaciones(grafo, ciudades, parametros):
+    """Recibe un grafo y una lista de parámetros, que contiene una
+    ciudad origen y un número entero n. Imprime un recorrido de n
+    lugares que comienza y finaliza en origen."""
+    if len(parametros) != 2 or not parametros[1].isdigit():
+        return False
+    origen, n = parametros[0], int(parametros[1])
+    if origen not in ciudades:
+        return False
+    recorrido = None
+    for aeropuerto in ciudades[origen]:
+        recorrido = b.obtener_viaje(grafo, aeropuerto, n)
+        if recorrido != None: break
+    if not recorrido:
+        print("No se encontro recorrido")
+        return True
+    print(" -> ".join(recorrido))
+    return recorrido
 
-def procesar_comando(grafo, ciudades, linea):
-	"""Recibe un grafo, un diccionario de ciudades con todos los
-	datos disponibles y una linea y procesa los comandos correspondientes.
-	Devuelve True en caso de éxito, False en caso de error."""
-	comando = linea.rstrip("\n").split(" ")
-	if comando[0] == "listar_operaciones":
-		listar_operaciones()
-		return True
-	if len(comando) < 2:
-		return False
-	parametros = " ".join(comando[1:]).split(",")
-	if comando[0] == "camino_mas" or comando[0] == "camino_escalas":
-		return camino_minimo(grafo, ciudades, parametros)
-	if comando[0] == "nueva_aerolinea":
-		return nueva_aerolinea(grafo, parametros)
-	if comando[0] == "vacaciones":
-		return vacaciones(grafo, parametros)
-	return False
+def exportar_kml(grafo, parametros, recorrido):
+    """Recibe un grafo, un recorrido y una lista de parámetros que contiene
+    la ruta del archivo kml a exportar. Devuelve True en caso de ejecutarse
+    correctamente. En caso de error o si el recorrido no es una lista,
+    devuelve False."""
+    if len(parametros) != 1:
+        return False
+    try:
+        with open(parametros[0], "w") as archivo:
+            b.exportar_archivo_kml(grafo, recorrido, archivo)
+        return True
+    except:
+        return False
+
+def procesar_comando(grafo, ciudades, linea, ultimo):
+    """Recibe un grafo, un diccionario de ciudades con todos los
+    datos disponibles y una linea y procesa los comandos correspondientes.
+    Devuelve True en caso de éxito, False en caso de error."""
+    comando = linea.rstrip("\n").split(" ")
+    if comando[0] == "listar_operaciones":
+        listar_operaciones()
+        return True
+    if len(comando) < 2:
+        return False
+    parametros = " ".join(comando[1:]).split(",")
+    if comando[0] == "camino_mas" or comando[0] == "camino_escalas":
+        return camino_minimo(grafo, ciudades, parametros)
+    if comando[0] == "nueva_aerolinea":
+        return nueva_aerolinea(grafo, parametros)
+    if comando[0] == "vacaciones":
+        return vacaciones(grafo, ciudades, parametros)
+    if comando[0] == "exportar_kml":
+        return exportar_kml(grafo, parametros, ultimo)
+    return False
 
 def main():
-	"""Funcion principal del programa, se encarga de cargar los datos
-	en memoria y ejecutar los comandos recibidos"""
-	ruta_aeropuertos = sys.argv[1]
-	ruta_vuelos = sys.argv[2]
-	grafo = Grafo()
-	ciudades = {}
-	obtener_aeropuertos(grafo, ciudades, ruta_aeropuertos)
-	obtener_vuelos(grafo, ruta_vuelos)
-	for linea in sys.stdin:
-		if procesar_comando(grafo, ciudades, linea):
-			print("OK")
-		else:
-			print("ERROR")
+    """Funcion principal del programa, se encarga de cargar los datos
+    en memoria y ejecutar los comandos recibidos"""
+    ruta_aeropuertos = sys.argv[1]
+    ruta_vuelos = sys.argv[2]
+    grafo = Grafo()
+    ciudades = {}
+    obtener_aeropuertos(grafo, ciudades, ruta_aeropuertos)
+    obtener_vuelos(grafo, ruta_vuelos)
+    ultimo = None
+    for linea in sys.stdin:
+        ultimo = procesar_comando(grafo, ciudades, linea, ultimo)
+        if ultimo:
+            print("OK")
+        else:
+            print("ERROR")
 
 main()
